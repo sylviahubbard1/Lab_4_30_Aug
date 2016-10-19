31b19b8ff96db7e725b9941ff7f49550f35f2b2c
sylviahubbard1 <sylviahubbard@gmail.com>
2016-10-19 10:54:48 -0400

    10/19/16

--- LabFourFactorialCalc/Program.cs
+++ LabFourFactorialCalc/Program.cs
@@ -4,12 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LabFourFactorialCalc
+namespace Lab_4_30_Aug
+
 {
     class Program
     {
+        static string ans;
+        static int valueInt;
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the Factorial Calculator!\n");
+            Console.Write("Enter an integer that's greater than 1 but less than 10 : ");
+
+            do
+            {
+               
+                                
+                valueInt = Int32.Parse(Console.ReadLine());
+                Console.WriteLine($"Factorial of {valueInt} is {Factorial(valueInt)}");
+              
+                                               
+                Console.Write("\nContinue ?(y/n) :");
+                ans = Console.ReadLine();
+                Console.WriteLine();
+                if (ans == "y")
+                    Console.Write($"Enter an integer that's greater than 1 but less than {Factorial(valueInt)}: ");
+            } while (ans == "y");
         }
+        static long Factorial(int num)
+        {
+            if (num == 0) return 1; return num * Factorial(num - 1);
+        }        //static  long FactorialValue(int num)        
+                   
     }
 }
